359bbbd946844b3808c6248d59b659ec2ccf8f3c
Fabien Leroy <leroy223@campus.unicaen.fr>
2023-09-11 09:55:19 +0200

    chrono

---
+++ script/script.js
@@ -0,0 +1,16 @@
+let temps = 15
+let travail = true
+let start
+const timerElement = document.getElementById("timer")
+const button = document.getElementById("start")
+
+setInterval(() => {
+    let minutes = parseInt(temps / 60, 10)
+    let secondes = parseInt(temps % 60, 10)
+  
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    secondes = secondes < 10 ? "0" + secondes : secondes
+  
+    timerElement.innerText = `${minutes}:${secondes}`
+    temps = temps <= 0 ? 0 : temps - 1
+  }, 1000)

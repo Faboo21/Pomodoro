b5c6ef98ceea3c5a425353ad0cfec226cb0eabaa
Fabien Leroy <leroy223@campus.unicaen.fr>
2023-09-12 09:18:25 +0200

    bouton

--- script/script.js
+++ script/script.js
@@ -4,13 +4,24 @@ let start
 const timerElement = document.getElementById("timer")
 const button = document.getElementById("start")
 
-setInterval(() => {
-    let minutes = parseInt(temps / 60, 10)
-    let secondes = parseInt(temps % 60, 10)
-  
-    minutes = minutes < 10 ? "0" + minutes : minutes
-    secondes = secondes < 10 ? "0" + secondes : secondes
-  
-    timerElement.innerText = `${minutes}:${secondes}`
-    temps = temps <= 0 ? 0 : temps - 1
-  }, 1000)
+let bouton = document.getElementById("start")
+let isStarted = false
+bouton.addEventListener('click', () => {
+  if (isStarted) {
+    location.reload();
+  }
+  else {
+    isStarted = true;
+    setInterval(() => {
+      let minutes = parseInt(temps / 60, 10)
+      let secondes = parseInt(temps % 60, 10)
+
+      minutes = minutes < 10 ? "0" + minutes : minutes
+      secondes = secondes < 10 ? "0" + secondes : secondes
+
+      timerElement.innerText = `${minutes}:${secondes}`
+      temps = temps <= 0 ? 0 : temps - 1
+    }, 1000)
+
+  }
+})

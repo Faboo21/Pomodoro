7c6cc96af7778af34bb545334b2560a442023397
Faboo <fabienleroy2004@gmail.com>
2023-09-14 17:23:12 +0200

    v2

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
-let tempsTravail = 5
-let tempsRepos = 25
+let tempsTravail = 0
+let tempsRepos = 0
 
+let temps = 1500
 let travail = false
 let bouton = document.getElementById("start")
 let isStarted = false
@@ -16,39 +17,49 @@ bouton.addEventListener('click', () => {
     isStarted = true;
     travail = true
     bouton.className = "fa-solid fa-arrows-rotate"
-
-    let temps = tempsTravail
+    tempsTravail = parseInt(document.getElementById('idTravail').value.split(':')[0]) * 3600 + parseInt(document.getElementById('idTravail').value.split(':')[1])*60 + parseInt(document.getElementById('idTravail').value.split(':')[2])
+    tempsRepos = parseInt(document.getElementById('idPause').value.split(':')[0]) * 3600 + parseInt(document.getElementById('idPause').value.split(':')[1])*60 + parseInt(document.getElementById('idPause').value.split(':')[2])
+    if (Number.isInteger(tempsTravail)){temps = tempsTravail}
+    
     setInterval(() => {
       if (temps <= 0 && travail){
         travail = false
-        temps = tempsRepos
+        if (Number.isInteger(tempsTravail)){temps = tempsRepos}
+        else {temps = 300}
         let etat = document.getElementById('etat')
         etat.innerText = 'Pause'
       }
       if (temps <= 0 && !travail){
         travail = true
-        temps = tempsTravail
+        if (Number.isInteger(tempsTravail)){temps = tempsTravail}
+        else {temps = 15000}
         let etat = document.getElementById('etat')
         etat.innerText = 'Travail'
       }
 
-      let minutes = parseInt(temps / 60, 10)
+      let heure = parseInt(temps / 3600, 10)
+      let minutes = parseInt(temps % 3600 / 60, 10)
       let secondes = parseInt(temps % 60, 10)
 
+      heure = heure < 10 ? "0" + heure : heure
       minutes = minutes < 10 ? "0" + minutes : minutes
       secondes = secondes < 10 ? "0" + secondes : secondes
       temps = temps <= 0 ? 0 : temps - 1
+      if (temps > 3600)
+      timerElement.innerText = `${heure}:${minutes}:${secondes}`
+      else 
       timerElement.innerText = `${minutes}:${secondes}`
 
-    }, 1000)
+      if (travail){
+        let etat = document.getElementById('etat')
+        etat.style.borderColor = "red"
+      }
+      if (!travail){
+        let etat = document.getElementById('etat')
+        etat.style.borderColor = "green"
+      }
+    }, 10)
   }
 })
 
-function Envoi() {
-// Récupérer la valeur des champs nom et email
-var formTravail = document.getElementById('idTravail').value;
-var formPause = document.getElementById('idEmail').value;
 
-document.getElementById('idNom').style.backgroundColor="#9C6";
-document.getElementById('idEmail').style.backgroundColor="#9C6";
-}

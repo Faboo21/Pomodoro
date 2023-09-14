188b3073f49f2b952f2d5c582acb2ce1e0889cac
Faboo <fabienleroy2004@gmail.com>
2023-09-14 10:25:17 +0200

    v1 manque style

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
-let tempsTravail = 25
-let tempsRepos = 5
+let tempsTravail = 5
+let tempsRepos = 25
 
 let travail = false
 let bouton = document.getElementById("start")
@@ -19,22 +19,27 @@ bouton.addEventListener('click', () => {
 
     let temps = tempsTravail
     setInterval(() => {
+      if (temps <= 0 && travail){
+        travail = false
+        temps = tempsRepos
+        let etat = document.getElementById('etat')
+        etat.innerText = 'Pause'
+      }
+      if (temps <= 0 && !travail){
+        travail = true
+        temps = tempsTravail
+        let etat = document.getElementById('etat')
+        etat.innerText = 'Travail'
+      }
+
       let minutes = parseInt(temps / 60, 10)
       let secondes = parseInt(temps % 60, 10)
 
       minutes = minutes < 10 ? "0" + minutes : minutes
       secondes = secondes < 10 ? "0" + secondes : secondes
-
-      timerElement.innerText = `${minutes}:${secondes}`
       temps = temps <= 0 ? 0 : temps - 1
-      if (temps <= 0 && travail){
-        travail = false
-        temps = tempsRepos
-      }
-      if (temps <= 0 && !travail){
-        travail = true
-        temps = tempsTravail
-      }
+      timerElement.innerText = `${minutes}:${secondes}`
+
     }, 1000)
   }
 })

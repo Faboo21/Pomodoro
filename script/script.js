86c0c8d2c3e511af61650989e86d6801707a79bf
Fabien Leroy <leroy223@campus.unicaen.fr>
2023-09-18 11:58:12 +0200

    localstorage

--- script/script.js
+++ script/script.js
@@ -1,6 +1,13 @@
 let tempsTravail = 0
 let tempsRepos = 0
 
+if (localStorage.getItem("inputTravail") != null) {
+  document.getElementById('idTravail').value = localStorage.getItem("inputTravail")
+}
+if (localStorage.getItem("inputPause") != null) {
+  document.getElementById('idPause').value = localStorage.getItem("inputPause")
+}
+
 let temps = 1500
 let travail = false
 let bouton = document.getElementById("start")
@@ -22,15 +29,18 @@ bouton.addEventListener('click', () => {
     let valise = document.getElementById("valise")
     valise.style.color = "rgb(0, 20, 65)"
     bouton.className = "fa-solid fa-arrows-rotate"
-    tempsTravail = parseInt(document.getElementById('idTravail').value.split(':')[0]) * 3600 + parseInt(document.getElementById('idTravail').value.split(':')[1])*60 + parseInt(document.getElementById('idTravail').value.split(':')[2])
-    tempsRepos = parseInt(document.getElementById('idPause').value.split(':')[0]) * 3600 + parseInt(document.getElementById('idPause').value.split(':')[1])*60 + parseInt(document.getElementById('idPause').value.split(':')[2])
-    if (Number.isInteger(tempsTravail)){temps = tempsTravail}
-    
+    tempsTravail = parseInt(document.getElementById('idTravail').value.split(':')[0]) * 3600 + parseInt(document.getElementById('idTravail').value.split(':')[1]) * 60 + parseInt(document.getElementById('idTravail').value.split(':')[2])
+    tempsRepos = parseInt(document.getElementById('idPause').value.split(':')[0]) * 3600 + parseInt(document.getElementById('idPause').value.split(':')[1]) * 60 + parseInt(document.getElementById('idPause').value.split(':')[2])
+    localStorage.setItem("inputTravail", document.getElementById('idTravail').value);
+    localStorage.setItem("inputPause", document.getElementById('idPause').value);
+
+    if (Number.isInteger(tempsTravail)) { temps = tempsTravail }
+
     setInterval(() => {
-      if (temps <= 0 && travail){
+      if (temps <= 0 && travail) {
         travail = false
-        if (Number.isInteger(tempsTravail)){temps = tempsRepos}
-        else {temps = 300}
+        if (Number.isInteger(tempsTravail)) { temps = tempsRepos }
+        else { temps = 300 }
         let pau = document.getElementById('pause')
         pau.style.color = "rgb(0, 20, 65)"
         pau.style.backgroundColor = "#FFFFFF"
@@ -42,10 +52,10 @@ bouton.addEventListener('click', () => {
         let tasse = document.getElementById('tasse')
         tasse.style.color = "rgb(0, 20, 65)"
       }
-      if (temps <= 0 && !travail){
+      if (temps <= 0 && !travail) {
         travail = true
-        if (Number.isInteger(tempsTravail)){temps = tempsTravail}
-        else {temps = 15000}
+        if (Number.isInteger(tempsTravail)) { temps = tempsTravail }
+        else { temps = 15000 }
         let tra = document.getElementById('travail')
         tra.style.color = "rgb(0, 20, 65)"
         tra.style.backgroundColor = "#FFFFFF"
@@ -67,18 +77,9 @@ bouton.addEventListener('click', () => {
       secondes = secondes < 10 ? "0" + secondes : secondes
       temps = temps <= 0 ? 0 : temps - 1
       if (temps > 3600)
-      timerElement.innerText = `${heure}:${minutes}:${secondes}`
-      else 
-      timerElement.innerText = `${minutes}:${secondes}`
-
-      if (travail){
-        let etat = document.getElementById('etat')
-        etat.style.borderColor = "red"
-      }
-      if (!travail){
-        let etat = document.getElementById('etat')
-        etat.style.borderColor = "green"
-      }
+        timerElement.innerText = `${heure}:${minutes}:${secondes}`
+      else
+        timerElement.innerText = `${minutes}:${secondes}`
     }, 1000)
   }
 })
